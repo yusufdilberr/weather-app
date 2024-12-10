fe7ec49d729f8cca3b9f098dc945def82a6f1574
yusufdilberr <yusuferendilber1@gmail.com>
2024-12-10 21:16:41 +0300

    Weather app with React

---
+++ src/api.js
@@ -0,0 +1,12 @@
+const url = '/places/%7BplaceId%7D/distance?toPlaceId=Q60';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '18934d5531msh42300e6ce23a453p10e6aejsna149bab60dd0',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL ='https://wft-geo-db.p.rapidapi.com/v1/geo';
+
